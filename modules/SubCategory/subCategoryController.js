7cbe0c3d63d22cd017cd3a10eb62363d3f8adec9
dosepehr <dosepehr@gmail.com>
2025-03-24 18:00:26 +0330

    return category name for sub category

--- modules/SubCategory/subCategoryController.js
+++ modules/SubCategory/subCategoryController.js
@@ -8,7 +8,12 @@ const {
 const SubCategory = require('./subCategoryModel');
 
 exports.addSubCategory = addOne(SubCategory);
-exports.getSubCategory = getOne(SubCategory);
+exports.getSubCategory = getOne(SubCategory, [
+    {
+        path: 'category',
+        select: 'name',
+    },
+]);
 exports.getSubCategories = getAll(SubCategory);
 exports.updateSubCategory = updateOne(SubCategory);
 exports.deleteSubCategory = deleteOne(SubCategory);
