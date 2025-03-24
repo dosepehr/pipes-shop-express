e37306eb83a1f755f9c70d94da20b88f28bd670c
dosepehr <dosepehr@gmail.com>
2025-03-24 18:32:29 +0330

    return products data for breadcrumb

--- modules/Product/productController.js
+++ modules/Product/productController.js
@@ -8,6 +8,15 @@ const {
 } = require('../../modules/Factory/factoryController');
 exports.addProduct = addOne(Product);
 exports.getProducts = getAll(Product);
-exports.getProduct = getOne(Product);
+exports.getProduct = getOne(Product, [
+    {
+        path: 'subCategory',
+        select: 'name',
+        populate: {
+            path: 'category',
+            select: 'name',
+        },
+    },
+]);
 exports.updateProduct = updateOne(Product);
 exports.deleteProduct = deleteOne(Product);
